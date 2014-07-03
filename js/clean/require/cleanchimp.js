e1cb3d3c4827012c1fcdea979b013155880371b2
Kalen Jordan <kalen@sparcksoft.com>
2014-07-03 08:19:42 -0700

    Fix the query string log

--- js/clean/require/cleanchimp.js
+++ js/clean/require/cleanchimp.js
@@ -3,11 +3,11 @@ define(['util', 'prototype-cookies', 'query-string'], function(util, cookies, qu
         run: function() {
             if (queryString.toObject().mc_cid) {
                 cookies.set('cleanchimp_campaign_id', queryString.toObject().mc_cid, 30);
-                util.log("Set mc_cid on cookie: ", queryString.toObject().mc_cid);
+                util.log("Set mc_cid on cookie: " + queryString.toObject().mc_cid);
             }
             if (queryString.toObject().mc_eid) {
                 cookies.set('cleanchimp_email_id', queryString.toObject().mc_eid, 30);
-                util.log("Set mc_eid on cookie: ", queryString.toObject().mc_eid);
+                util.log("Set mc_eid on cookie: " + queryString.toObject().mc_eid);
             }
         }
     }
