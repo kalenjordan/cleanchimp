6c38e3cd9ff8b3a6e3bdb193c67aa396d2257d9d
Kalen Jordan <kalen@sparcksoft.com>
2014-07-15 15:10:55 -0700

    Add the log file name to the logServer call

--- js/clean/require/cleanchimp.js
+++ js/clean/require/cleanchimp.js
@@ -3,12 +3,12 @@ define(['util', 'prototype-cookies', 'query-string'], function(util, cookies, qu
         run: function() {
             if (queryString.toObject().mc_cid) {
                 cookies.set('cleanchimp_campaign_id', queryString.toObject().mc_cid, 30);
-                util.logServer("Set mc_cid on cookie: " + queryString.toObject().mc_cid)
+                util.logServer("Set mc_cid on cookie: " + queryString.toObject().mc_cid, 'cleanchimp');
             }
 
             if (queryString.toObject().mc_eid) {
                 cookies.set('cleanchimp_email_id', queryString.toObject().mc_eid, 30);
-                util.logServer("Set mc_eid on cookie: " + queryString.toObject().mc_eid)
+                util.logServer("Set mc_eid on cookie: " + queryString.toObject().mc_eid, 'cleanchimp');
             }
         }
     }
